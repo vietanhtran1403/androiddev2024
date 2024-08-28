6204619b2545ccfbe7a2b2fd152449924c7734f0
vietanhtran1403 <vietanhtran140323@gmail.com>
2024-08-28 15:37:32 +0700

    practical 3

--- andproject/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ andproject/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -3,7 +3,6 @@ package vn.edu.usth.weather;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,47 +10,53 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class WeatherActivity extends AppCompatActivity {
     private static final String WA = "WeatherActivity";
+    ForecastFragment forecastFragment = new ForecastFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.main, forecastFragment).commit();
+
         setContentView(R.layout.activity_weather);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
-        Log.i(WA, "onCreate called");
+        Log.i(WA, "onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(WA, "onStart called");
+        Log.d(WA, "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(WA, "onResume called");
+        Log.i(WA, "onResume");
     }
 
     @Override
     protected void onPause(){
         super.onPause();
-        Log.i(WA, "onPause called");
+        Log.i(WA, "onPause");
     }
     @Override
     protected void onStop(){
         super.onStop();
-        Log.i(WA, "onStop called");
+        Log.i(WA, "onStop");
     }
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        Log.i(WA, "onDestroy called");
+        Log.i(WA, "onDestroy");
     }
 }
 
