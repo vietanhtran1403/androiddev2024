fedc2662add571fedfc71b8de79d3ae0ed47dd74
vietanhtran1403 <vietanhtran140323@gmail.com>
2024-09-18 14:35:21 +0700

    prac 9

--- andproject/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ andproject/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -4,29 +4,18 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-
-import com.google.android.material.tabs.TabLayout;
 public class WeatherActivity extends AppCompatActivity {
-    private ViewPager viewPager;
-
     private static final String Tag = "WeatherActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //        ForecastFragment forecastFragment = new ForecastFragment();
+//
+//        getSupportFragmentManager().beginTransaction().add(
+//                R.id.main, forecastFragment).commit();
         setContentView(R.layout.activity_weather);
 
-        HomeFragmentPagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
-
-
-        ViewPager pager = (ViewPager) findViewById(R.id.pager);
-        pager.setOffscreenPageLimit(3);
-        pager.setAdapter(adapter);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
-        tabLayout.setupWithViewPager(pager);
-
-        Log.i(Tag, "onCreate");
+        Log.i(Tag, "Create");
     }
 
     @Override
@@ -34,28 +23,14 @@ public class WeatherActivity extends AppCompatActivity {
         super.onStart();
         Log.i(Tag, "Start");
     }
-
     @Override
     protected void onResume() {
         super.onResume();
         Log.i(Tag, "Resume");
     }
-
     @Override
     protected void onPause() {
         super.onPause();
         Log.i(Tag, "Pause");
-    }
-
-    @Override
-    protected void onStop() {
-        Log.i(Tag, "Stop");
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(Tag, "Destroy");
     }
 }
