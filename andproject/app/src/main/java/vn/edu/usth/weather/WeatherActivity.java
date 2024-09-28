61dbbe5ba153bee67842cb827a6a12962381b540
vietanhtran1403 <vietanhtran140323@gmail.com>
2024-09-28 15:08:33 +0700

    practical 11

--- andproject/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ andproject/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,10 +1,21 @@
 package vn.edu.usth.weather;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
 public class WeatherActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
     private static final String Tag = "WeatherActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +25,18 @@ public class WeatherActivity extends AppCompatActivity {
 //        getSupportFragmentManager().beginTransaction().add(
 //                R.id.main, forecastFragment).commit();
         setContentView(R.layout.activity_weather);
+        PagerAdapter adapter = new vn.edu.usth.weather.HomeFragmentPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = findViewById(R.id.pager);
+
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(pager);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.audio1);
+
+        mediaPlayer.start();
 
         Log.i(Tag, "Create");
     }
@@ -32,5 +55,41 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.i(Tag, "Pause");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        Log.i("weather", "Destroy");
+    }
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+        private final String[] titles = new String[]{"First", "Second", "Third"};
+
+        public HomeFragmentPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int page) {
+
+            return new WeatherAndForecastFragment();
+
+            //return new WeatherAndForecastFragment();
+        }
+        public CharSequence getPageTitle(int page) {
+            // returns a tab title corresponding to the specified page
+            return titles[page];
+        }
     }
 }
