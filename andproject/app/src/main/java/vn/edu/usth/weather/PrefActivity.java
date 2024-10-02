87695c0e34d0c85a5c1ca8f6f629cc4b9944bf88
vietanhtran1403 <vietanhtran140323@gmail.com>
2024-10-02 14:45:50 +0700

    update Practical 12

---
+++ andproject/app/src/main/java/vn/edu/usth/weather/PrefActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+public class PrefActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pref);
+    }
+}
